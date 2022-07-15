d8ea21c9d316d25e31d723a04ec9c50586dcf0a3
José Freites <jlfu2004@gmail.com>
2022-07-15 08:52:46 -0300

    Better Comments

--- js/cars.js
+++ js/cars.js
@@ -1,11 +1,11 @@
 "use strict";
-// ? ● Exercício 1:
+// ! ● Exercício 1:
+// ? 45%, escrever um algoritmo que leia o custo de fábrica de um carro e escreva o custo ao
+// ? consumidor.
 // O custo ao consumidor de um carro novo é a soma do custo de fábrica com a percentagem
 // do distribuidor e dos
 // impostos (aplicados ao custo de fábrica). Supondo que a percentagem do distribuidor seja
 // de 28% e os impostos de
-// 45%, escrever um algoritmo que leia o custo de fábrica de um carro e escreva o custo ao
-// consumidor.
 let manofactoryCost = 5000;
 const dealerCost = 0.28;
 const tax = 0.45;
@@ -14,8 +14,8 @@ function calcValue(manofactoryCost, dealerCost, tax) {
 }
 let totalCost = calcValue(manofactoryCost, dealerCost, tax);
 document.write(`Precio del carro: ${totalCost}<br/>`);
-// ? ● Exercício 2:
-// Faça um algoritmo que leia as 3 notas de um aluno e calcule a média final deste aluno.
+// ! ● Exercício 2:
+// ? Faça um algoritmo que leia as 3 notas de um aluno e calcule a média final deste aluno.
 // Considerar que a média é
 // ponderada e que o peso das notas é: 2,3 e 5.
 let note1 = 10;
@@ -30,8 +30,8 @@ function calcGradeAverage(note1, note2, note3, weight1, weight2, weight3) {
 }
 let finalGrade = calcGradeAverage(note1, note2, note3, weight1, weight2, weight3);
 document.write(`Nota final: ${finalGrade}<br/>`);
-// ? ● Exercício 3:
-// Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e
+// ! ● Exercício 3:
+// ? Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e
 // mostre-a expressa apenas em
 // dias.
 let years = 20;
@@ -40,8 +40,8 @@ let days = 10;
 // Arrow function to get age in days
 let ageInDays = (years, months, days) => years * 365 + months * 30 + days;
 document.write(`La edad en días es: ${ageInDays(years, months, days)}<br/>`);
-// ● Exercício 4:
-// Escreva um algoritmo que leia uma lista de números inteiros e mostre o maior deles.
+// ! ● Exercício 4:
+// ? Escreva um algoritmo que leia uma lista de números inteiros e mostre o maior deles.
 let array = [2, 3, 6, 2, 5, 7, 9, 10, 42, 22, 66, 33, 77, 11];
 let biggestNumber = calcBgNumber(...array);
 document.write(`El número más grande es: ${biggestNumber}<br/>`);
@@ -54,8 +54,8 @@ function calcBgNumber(...numbers) {
     }
     return biggestNumber;
 }
-// ● Exercício 5:
-// Escreva um algoritmo que leia uma lista de números inteiros e retorne os números prims.
+// ! ● Exercício 5:
+// ? Escreva um algoritmo que leia uma lista de números inteiros e retorne os números prims.
 var PnQuantity = 50;
 var numPrimeStart = 2;
 var primeNumbers = [];
